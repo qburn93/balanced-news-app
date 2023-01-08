11af0e62ed61b2e8cbc7d1104daad09d7b3d34ab
Bogdan <bogdan.gugu93@gmail.com>
2023-01-08 19:50:37 +0000

    Add political leaning to news post info

--- news/views.py
+++ news/views.py
@@ -7,3 +7,18 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 6
+
+
+# class PostLeaning:
+#    def __init__(self, post):
+#        self.post = post
+#        self.leaning = self.determine_leaning()
+#    
+#    def determine_leaning(self):
+#        # Check the value of the "admin_leaning" field in the post model
+#        if self.post.political_view == "Left":
+#            return "Left"
+#        elif self.post.political_view == "Right":
+#            return "Right"
+#        else:
+#            return "Neutral"
