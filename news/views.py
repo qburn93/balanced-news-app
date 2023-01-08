287e179a0ea1a3d4010954fad16e14552a69d52a
Bogdan <bogdan.gugu93@gmail.com>
2023-01-08 18:47:55 +0000

    Created templates and urls pathing for home page

--- news/views.py
+++ news/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 6
--- newstar/urls.py
+++ newstar/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote', include('django_summernote.urls')),
+    path('', include('news.urls'), name='news_urls'),
 ]
