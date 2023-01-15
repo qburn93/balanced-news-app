77e787108bd7f9247c871a6f45eb0f0c47db622c
Bogdan <bogdan.gugu93@gmail.com>
2023-01-15 00:00:37 +0000

    add category templates, currently having issues with pathing

--- news/urls.py
+++ news/urls.py
@@ -4,6 +4,7 @@ from .views import sports, economy
 
 urlpatterns = [
     path("", views.PostList.as_view(), name='home'),
+    path('<slug:slug>/', views.NewsPostDetail.as_view(), name='news_detail'),
     path('sports/', sports, name='sports'),
     path('economy/', economy, name='economy'),
     
--- news/views.py
+++ news/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views import generic
+from django.views import generic, View
 from .models import Post
 
 class PostList(generic.ListView):
@@ -9,16 +9,37 @@ class PostList(generic.ListView):
     paginate_by = 6
 
 
+class NewsPostDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            'news_detail.html',
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked
+            },
+        )
+
+
 def post_detail(request, id):
     post = get_object_or_404(Post, id=id)
     if post.category == 'SPORTS':
         return render(request, 'blog/sports_post_detail.html', {'post': post})
     elif post.category == 'ECONOMY':
-        return render(request, 'blog/economy_post_detail.html', {'post': post})
+        return render(request, 'economy_post_detail.html', {'post': post})
     elif post.category == 'POLITICAL':
-        return render(request, 'blog/political_post_detail.html', {'post': post})
+        return render(request, 'post_detail.html', {'post': post})
     else:
-        return redirect('home')
+        return render(request, 'blog/default_post_detail.html', {'post': post})
 
 
 def sports(request):
@@ -26,4 +47,4 @@ def sports(request):
 
 
 def economy(request):
-    return render(request, 'economy.html')
+    return render(request, 'economy_post_detail.html')
