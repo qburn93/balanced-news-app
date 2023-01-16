a249e5477b96ee370fa81fa36401d7baaf8cb8e6
Bogdan <bogdan.gugu93@gmail.com>
2023-01-16 16:48:15 +0000

    Add django--allauth, and set ups urls in base template for redirects

--- newstar/settings.py
+++ newstar/settings.py
@@ -40,12 +40,21 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
     'news',
 ]
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- newstar/urls.py
+++ newstar/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('news.urls'), name='news_urls'),
+    path('accounts/', include('allauth.urls')),
     
 ]
