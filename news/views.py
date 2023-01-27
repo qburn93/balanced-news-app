ac6661d7ca68b25348bfa723d0d0b68f8fbd880e
Bogdan <bogdan.gugu93@gmail.com>
2023-01-27 07:48:23 +0000

    Updated paginate by number

--- news/views.py
+++ news/views.py
@@ -9,7 +9,7 @@ class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
-    paginate_by = 6
+    paginate_by = 3
 
 
 class NewsPostDetail(View):
