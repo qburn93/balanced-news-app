7994324f173c3e1ae9bcbcf3495baba3c3cbd5a0
Bogdan <bogdan.gugu93@gmail.com>
2023-03-31 12:59:25 +0000

    add fix for correct category render

--- newstar/settings.py
+++ newstar/settings.py
@@ -19,7 +19,7 @@ if os.path.isfile('env.py'):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'news', 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
